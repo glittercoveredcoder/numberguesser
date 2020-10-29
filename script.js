3f1b0df578967e6a5f5d3fd560406956ecc4a682
glittercoveredcoder <alyssalrd@gmail.com>
2020-10-29 17:50:23 -0600

    wrote 4 functions to make game operationsal

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+  return Math.floor(Math.random()*9);
+};
+
+const compareGuesses = (human, comp, target) => {
+  let howCloseHuman = Math.abs(human - target);
+  let howCloseComp = Math.abs(comp - target);
+  if (howCloseHuman < howCloseComp) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const updateScore = winner => {
+  switch(winner) {
+    case 'human': 
+      humanScore += 1;
+      break;
+    case 'computer':
+      computerScore += 1;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber += 1;
+}
